d3118441b00d15553ecf0e85b96dea105b6068a1
Pacifique Twagirayesu <pacifiquetwagirayesu520@gmail.com>
2025-12-19 05:57:43 +0800

    feat(jwt): add jwt service

--- src/main/java/org/commitlink/procure/controller/HomeController.java
+++ src/main/java/org/commitlink/procure/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.commitlink.procure.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.commitlink.procure.services.impl.JwtService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,13 @@ import static org.commitlink.procure.utils.Constants.SWAGGER_URL;
 
 @Controller
 @RequestMapping
+@AllArgsConstructor
 public class HomeController {
+    private final JwtService jwtService;
+
     @GetMapping
     public void homePage(HttpServletResponse httpServletResponse) throws IOException {
+        System.out.println("jwtService = " + jwtService.getJwtSecretKey());
         httpServletResponse.sendRedirect(SWAGGER_URL);
 
     }
